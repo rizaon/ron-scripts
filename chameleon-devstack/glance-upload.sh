35832c70efcbff9b579c1ed7b06bc40a32c74a42
Riza Suminto <riza.on@gmail.com>
2017-04-05 00:53:43 +0000

    small bug fix

--- chameleon-devstack/glance-upload.sh
+++ chameleon-devstack/glance-upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-openstack create ubuntu-14.04-java --container-format bare --disk-format qcow2 --file ubuntu-14.04-java.qcow2
+openstack image create ubuntu-14.04-java --container-format bare --disk-format qcow2 --file ubuntu-14.04-java.qcow2
