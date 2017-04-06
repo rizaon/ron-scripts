3845f241a259c731b67da8820bcfdc4c9b45bcaf
Riza Suminto <riza.on@gmail.com>
2017-04-06 00:21:00 +0000

    append line to /etc/exports as root

--- chameleon-devstack/install-nfs-kernel-server.sh
+++ chameleon-devstack/install-nfs-kernel-server.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get install nfs-kernel-server
 sudo service nfs-kernel-server stop
-sudo echo "/home/ubuntu       10.1.0.0/16(rw,sync,no_subtree_check)" >> /etc/exports
+sudo sh -c 'echo "/home/ubuntu       10.1.0.0/16(rw,sync,no_subtree_check)" >> /etc/exports'
 sudo service nfs-kernel-server start
 
